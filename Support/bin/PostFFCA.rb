6328aed05c573b8eb629f18e41083499bdafa2b4
William Garrison <wgarrison@exacttarget.com>
2012-11-21 11:19:36 -0800

    modified code for 3 environments

--- Support/bin/PostFFCA.rb
+++ Support/bin/PostFFCA.rb
@@ -4,14 +4,16 @@ require "net/http"
 etCA = ARGV[0]
 etUser = ARGV[1] 
 etPass = ARGV[2]
+etEnv = ARGV[3]
 
 htmlDoc = STDIN.read
 params = {
 	'name' => etCA,
 	'user' => etUser,
 	'pass' => etPass,
+	'env' => etEnv,
 	'amp' => htmlDoc
 }
 
-response = Net::HTTP.post_form(URI.parse('http://www.solconlabs.com/textmate/API/ContentAreaFreeForm.php'), params)
+response = Net::HTTP.post_form(URI.parse('http://www.imhlabs.com/AMPscript-bundle/API/ContentAreaFreeForm.php'), params)
 puts response.body
